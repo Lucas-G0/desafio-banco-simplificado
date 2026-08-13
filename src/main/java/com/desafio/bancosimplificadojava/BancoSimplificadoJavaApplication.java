7b938727c8518ef92a5a747381614b1bfb36a05a
Lucas Goncalves <lucas.goncalves@db1.com.br>
2026-08-13 16:52:52 -0300

    chore: initial commit

---
+++ src/main/java/com/desafio/bancosimplificadojava/BancoSimplificadoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.bancosimplificadojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoSimplificadoJavaApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BancoSimplificadoJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/desafio/bancosimplificadojava/BancoSimplificadoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.bancosimplificadojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoSimplificadoJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
